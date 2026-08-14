c81154a48066d68c034551731b260f14da71eb7e
MarcRudorfer <91831301+MarcRudorfer@users.noreply.github.com>
2026-08-14 02:45:59 +0200

    Fix exact DIAG4Q UI anchor text

--- tools/diag4q_post_sensor_markers.py
+++ tools/diag4q_post_sensor_markers.py
@@ -6,7 +6,7 @@ def mark(txt):
     return '#ifdef DISPLAY_CLASS\n  if (disp) { disp->startFrame(); disp->drawTextCentered(disp->width()/2, 28, "'+txt+'"); disp->endFrame(); }\n#endif\n'
 
 gps='#if ENV_INCLUDE_GPS == 1\n  the_mesh.applyGpsPrefs();\n#endif\n'
-ui='  ui_task.begin(disp, &sensors, the_mesh.getNodePrefs());  // still want to pass this in as dependency, as prefs may be moved\n'
+ui='  ui_task.begin(disp, &sensors, the_mesh.getNodePrefs());  // still want to pass this in as dependency, as prefs might be moved\n'
 boot='  board.onBootComplete();\n'
 if gps not in s: raise SystemExit('GPS anchor missing')
 if ui not in s: raise SystemExit('UI anchor missing')
